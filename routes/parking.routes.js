86ac7b027186f413cec4bf12bc1170d0ccdccd1a
chandrajyotisaikia <77949655+chandrajyotisaikia@users.noreply.github.com>
2026-08-07 07:23:54 +0530

    Add files via upload

--- routes/parking.routes.js
+++ routes/parking.routes.js
@@ -8,6 +8,8 @@ router.get('/check-subscriber/:plate', ctrl.quickCheckSubscriber);
 router.get('/entries', ctrl.getEntries);
 router.get('/entries/active', ctrl.getActiveEntries);
 router.post('/entries/:id/exit', ctrl.markExit);
+router.get('/settings', ctrl.getSettingsHandler);
+router.post('/settings', ctrl.postSettingsHandler);
 router.post('/subscribers', ctrl.postSubscriber);
 router.get('/subscribers', ctrl.getSubscribers);
 router.get('/subscribers/expiring', ctrl.getExpiringSubscribers);
